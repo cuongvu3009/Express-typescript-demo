d8e90096a141671344b462b1448f1fde9900ea69
cuongvu3009 <cuongvu3009@gmail.com>
2022-12-05 22:11:08 +0200

    feat: comparePassword and Hash password function

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,5 @@ export default {
   port: 1337,
   dbUri:
     'mongodb+srv://cuong:1234@nodeexpressproject.nlzou.mongodb.net/Node-typescript-demo?retryWrites=true&w=majority',
+  saltWorksFactor: 10,
 };
---
+++ src/models/user.model.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from 'config';
+
+interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<Boolean>;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+//	hash password function
+//	 (err?: Error) => void is a whole code of HookNextFunction which imported from mongoose, which no longer exported
+userSchema.pre('save', async function (next: (err?: Error) => void) {
+  let user = this as UserDocument;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>('saltWorksFactor'));
+
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+//	compare password function
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
